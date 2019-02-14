53c61fc52f459a46e4318e2e11bde60137ead7b4
Keanna <keannakr@hawaii.edu>
2019-02-14 10:30:18 -1000

    Completed Swift Playgrounds Three Part One

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -2,7 +2,27 @@ import UIKit
 
 var str = "Hello, playground"
 
-var fruitNames : [String]
-fruitNames = ["Kiwi","Apples","Watermelon"]
+//Part 2
+//Assign fruitNames with 3 of your favorite fruits all in one line
 
+var fruitNames : [String] = ["Strawberries", "Bananas", "Tangerines"]
+
+//Part 4
 print(fruitNames)
+
+//Part 5 string interpolation; this is not an iteration
+print("I like to eat \(fruitNames)")
+
+//Part 6 name is like a placeholder; like a temporary container
+for name in fruitNames
+{
+    print("I like to eat" + name)
+}
+
+//Part 7 Index is where a certain item sits in an array i.e., Index 0 is Strawberries
+//print "name" at index 1 in fruitNames
+print(fruitNames[1])
+print(fruitNames[0])
+
+//Part 8, Start of Assignment #4
+
